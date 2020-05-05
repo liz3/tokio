6ef94aed46399b3081740e8e6d75ec64b9eb9fd6
Liz3 <accs@liz3.net>
2020-05-05 17:23:43 +0200

    add setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+git submodule update --init
+cd IXWebSocket
+mkdir build && cd build
+cmake -DUSE_TLS=1 ..
+make -j
+if [ $(uname) == "Darwin" ]
+then
+    make install
+else
+    sudo make install
+fi
+cd ../../
+npm install
